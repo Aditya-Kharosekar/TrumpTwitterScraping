031a4e42d83bcc5f0b49a45e310df5c62d7b5ba0
Aditya Kharosekar <adityakharosekar@gmail.com>
2017-01-08 16:09:36 +0530

    Pulled last 3250 Trump tweets. Should be enough to work with

--- TwitterScraping.py
+++ TwitterScraping.py
@@ -3,12 +3,6 @@ import json
 from tweepy import OAuthHandler
 import codecs
 
-#def process_or_store(tweet):
-#    print(json.dumps(tweet))
-#    print()
-#    with open("random.txt", 'w') as outfile:
-#        json.dump(tweet.text, outfile, sort_keys = True, indent=4)
-
 consumer_key = "i387QW7Eqgh12UHmK3VoQO9K5"
 consumer_secret = "BQI8c5eKale4etdA21mawnFqOmAziDQpnThm679V7UtLjbWlMG"
 access_token = "816857419338764288-S8Ay111O2Mo32QAs88tSnv5uKvmGCkF"
@@ -19,14 +13,22 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-stuff = api.user_timeline(screen_name="realDonaldTrump", count=200)
+all_tweets = []
 
-file = codecs.open("random.txt", "a", "utf-8")
-count=1
-for status in stuff:
-    #print(status.text)
-    #print()
-    file.write(status.text)
+new_tweets = api.user_timeline(screen_name="realDonaldTrump", count=200)
 
-#for tweet in tweepy.Cursor(api.home_timeline).items(10):
-#    process_or_store(tweet._json)
+#all_tweets.extend(new_tweets)
+#oldest = all_tweets[-1].id-1
+
+t = new_tweets[0];
+print(t.text)
+print(t.created_at)
+
+#while len(new_tweets) > 0:
+#    new_tweets = api.user_timeline(screen_name = "realDonaldTrump", count=200, max_id = oldest)
+#    all_tweets.extend(new_tweets)
+#    oldest = all_tweets[-1].id-1;
+
+file = codecs.open("random2.txt", "a", "utf-8")
+for tweet in all_tweets:
+    file.write(tweet.text)
